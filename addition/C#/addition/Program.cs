9b2d8c25f9598c6b11ddb5ec87d95be49897e02e
XHSKR <mail@xhs.kr>
2023-08-31 17:56:52 +0900

    add c# code for addition

---
+++ addition/C#/addition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace additiion
+{
+    class additiion
+    {
+        [DllImport("D:\\Study\\C\\dll\\addition\\C\\test.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int add_numbers(int a, int b);
+
+        static void Main(string[] args)
+        {
+            int result = add_numbers(5, 7);
+            Console.WriteLine("C# Result: " + result);
+        }
+    }
+}
